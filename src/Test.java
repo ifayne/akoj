7a984892658e198384635f111485e9c2b9e958fe
Fayne <ifayne@163.com>
2016-01-01 20:25:16 +0800

    Delete Test.java

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Test {
-	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
-		System.out.println("Hello");
-		cin.close();
-	}
-}
